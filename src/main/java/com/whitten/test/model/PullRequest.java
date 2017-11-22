0009714168f1bd7d7549874f83e61b6d25266dbd
zwhitten <zwhitten@gmail.com>
2017-11-22 14:29:16 -0500

    adding getters/setters for the PullRequest POJO

--- src/main/java/com/whitten/test/model/PullRequest.java
+++ src/main/java/com/whitten/test/model/PullRequest.java
@@ -13,4 +13,44 @@ public class PullRequest {
 
     private Date created_at;
     private Date closed_at;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getClosed_at() {
+        return closed_at;
+    }
+
+    public void setClosed_at(Date closed_at) {
+        this.closed_at = closed_at;
+    }
 }
